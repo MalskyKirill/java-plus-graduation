bcdd6b780a51f26c64cc2f0b349a103a0504d0dd
Kirill Malskiy <sabbat@mail.ru>
2025-05-30 14:38:30 +0300

    пакет логики category вынес в отдельный микросервис event-service

---
+++ core/event-service/src/main/java/ru/practicum/EventApp.java
@@ -0,0 +1,9 @@
+package ru.practicum;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class EventApp {
+}
